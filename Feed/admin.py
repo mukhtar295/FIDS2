cb8e3ce29c9ce760dbad6972d838176613a6704f
mukhtar295 <engr.mukhtar295@gmail.com>
2024-03-23 11:31:05 +0500

    admin and model files edited

--- Feed/admin.py
+++ Feed/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Airport, Airline, Flight
 # Register your models here.
+admin.site.register(Airport)
+admin.site.register(Airline)
+admin.site.register(Flight)
--- Feed/models.py
+++ Feed/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+
+class Airline(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+
+class Flight(models.Model):
+    airline = models.ForeignKey(Airline, on_delete=models.CASCADE)
+    flight_number = models.CharField(max_length=10)
+    departure_airport = models.ForeignKey(Airport, related_name='departure_flights', on_delete=models.CASCADE)
+    arrival_airport = models.ForeignKey(Airport, related_name='arrival_flights', on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.airline} - {self.flight_number}"
+
